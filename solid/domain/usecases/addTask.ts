74054dd63fa55bcc2bd647ecfbff43f73b9851db
kajirikajiri <kajirikajiri@gmail.com>
2021-01-03 20:45:41 +0900

    :sparkles: (domain/usecase) add new usecase

--- solid/domain/usecases/addTask.ts
+++ solid/domain/usecases/addTask.ts
@@ -1,1 +1,14 @@
 import {TaskModel} from 'domain/models'
+
+export interface AddTask {
+  add: (params: AddTask.Params) => AddTask.Model
+}
+
+export namespace AddTask {
+  export type Params = {
+    text: string
+    data: string
+    id: string
+  }
+  export type Model = TaskModel
+}
