6c4c604aa1c21863f090a766a307a6e08ca5a073
kajirikajiri <kajirikajiri@gmail.com>
2021-01-03 21:31:41 +0900

    :truck: (mv task)

--- solid/presentation/pages/tasks/taskList/index.tsx
+++ solid/presentation/pages/tasks/taskList/index.tsx
@@ -1,6 +1,6 @@
 import { TaskModel } from 'domain/models'
 import React, {FC} from 'react'
-import { Task } from '../task'
+import { Task } from './task'
 
 export const TaskList:FC<{tasks: TaskModel[]}> = ({tasks}) => {
   return (
