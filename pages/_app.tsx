23a2f90beebe453a23d1ff7a6579a8e6023376a6
kajirikajiri <kajirikajiri@gmail.com>
2021-01-03 20:43:01 +0900

    :label: (_app.tsx) add types

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import "tailwindcss/tailwind.css";
 import React from 'react'
+import {AppProps} from 'next/app'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }:AppProps) {
   return <Component {...pageProps} />
 }
 
